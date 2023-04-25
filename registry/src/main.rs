99328b551bbac9126afee374a1379a809709096d
Luke Chambers <consolelogluke@gmail.com>
2023-04-25 04:41:37 -0400

    Improve log format consistency between levels

--- registry/src/main.rs
+++ registry/src/main.rs
@@ -1,6 +1,6 @@
 use anyhow::{Context, Result};
 use log::LevelFilter;
-use simplelog::{ColorChoice, ConfigBuilder, TermLogger, TerminalMode};
+use simplelog::{ColorChoice, ConfigBuilder, TermLogger, TerminalMode, ThreadLogMode};
 
 fn main() -> Result<()> {
     init_logging().context("failed to initialize logging")?;
@@ -11,6 +11,9 @@ fn init_logging() -> Result<()> {
     TermLogger::init(
         LevelFilter::Debug,
         ConfigBuilder::new()
+            .set_thread_level(LevelFilter::Error)
+            .set_target_level(LevelFilter::Trace)
+            .set_thread_mode(ThreadLogMode::Both)
             .set_time_offset_to_local()
             .unwrap_or_else(|builder| builder)
             .build(),

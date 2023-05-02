9b36337ba1ee9e6fe10c9d8d5b9fb1a707ad54e7
Luke Chambers <consolelogluke@gmail.com>
2023-05-02 01:02:01 -0400

    Move `Cardstock` to cardstock-api

--- server-ext/src/main/java/sh/lpx/cardstock/Cardstock.java
+++
@@ -1,9 +0,0 @@
-package sh.lpx.cardstock;
-
-import org.jetbrains.annotations.NotNull;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-public class Cardstock {
-    public static final @NotNull Logger LOGGER = LoggerFactory.getLogger("Cardstock");
-}
